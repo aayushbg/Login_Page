bf5b44498d9b53667140e14e7b7c798c1de6ee3d
gh05tm4n <pv03158@gmail.com>
2019-03-19 15:32:25 +0530

    Signup form UI completed

--- frontend/js/signup-form.js
+++ frontend/js/signup-form.js
@@ -12,6 +12,14 @@ const toggle_student = () => {
 
         toggle_switch_student.classList.replace('btn-outline-primary','btn-primary');
         toggle_switch_faculty.classList.replace('btn-primary','btn-outline-primary');
+
+        // Swaping Faculty_ID with Enrollment Number
+        let enroll_num = '<label class="text-primary"><small>Enrollment number</small></label><input class="form-control" placeholder="Enrollment number" id="enroll_num">';
+        $('#enroll_or_faculty_id').empty();
+        $('#enroll_or_faculty_id').append(enroll_num);
+        // Adding Programme Selection
+        let program = '<div class="col-md"><label class="text-primary"><small>Programme</small></label><select class="custom-select text-muted"><option selected>Choose...</option><option value="BTech">BTech</option><option value="MBA">MBA</option><option value="BBA">BBA</option></select></div>';
+        $('#program_or_dept').append(program);
     }
 }
 
@@ -25,5 +33,15 @@ const toggle_faculty = () => {
 
         toggle_switch_student.classList.replace('btn-primary','btn-outline-primary');
         toggle_switch_faculty.classList.replace('btn-outline-primary','btn-primary');
+
+        // Swaping Enrollment Number with Faculty_ID
+        let faculty_id = '<label class="text-primary"><small>Faculty ID</small></label><input class="form-control" placeholder="Faculty ID" id="faculty_id">';
+        $('#enroll_or_faculty_id').empty();
+        $('#enroll_or_faculty_id').append(faculty_id);
+
+        // Removing Programme Selection
+        $('#program_or_dept').empty();
+        
     }
 }
+
